7b83a7fd63615de2e75b1b4fdd4e8326e297b6ef
robin <lb.robin1991@gmail.com>
2017-10-01 12:04:56 +0800

    customize

--- source/js/src/bootstrap.js
+++ source/js/src/bootstrap.js
@@ -49,4 +49,41 @@ $(document).ready(function () {
   CONFIG.motion.enable && NexT.motion.integrator.bootstrap();
 
   $(document).trigger('bootstrap:after');
+
+  // setTimeout(function (){
+  //   var adsHTML = function (slot){
+  //     return '<div align="center"><ins class="adsbygoogle"'+
+  //     'style="display:block"'+
+  //     'data-ad-client="ca-pub-8386186071288063"'+
+  //     'data-ad-slot="'+ slot +'"'+
+  //     'data-ad-format="auto"></ins></div>';
+  //   };
+  //   var bigsquare = function (slot){
+  //     return '<ins class="adsbygoogle"'+
+  //       'style="display:inline-block;width:336px;height:280px"'+
+  //       'data-ad-client="ca-pub-8386186071288063"'+
+  //       'data-ad-slot="'+slot+'"></ins>';
+  //   };
+  //   var addSlots = function (el, slot, type){
+  //     el[type](adsHTML(slot));
+  //     (adsbygoogle = window.adsbygoogle || []).push({});
+  //   }
+  //   if (NexT.utils.isDesktop()){
+  //     // sidebar
+  //     // addSlots($('#sidebar').find('.sidebar-inner'), 6065100430, 'append');
+  //     // <!-- bigsquare -->
+  //     var bigsquare1 = $(bigsquare(1697862430));
+  //     $("#comments").after(bigsquare1);
+  //     bigsquare1.wrap('<div style="float:left;"></div>');
+  //     (adsbygoogle = window.adsbygoogle || []).push({});
+  //     // <!-- bigsquare2 -->
+  //     var bigsquare2 = $(bigsquare(9860257630));
+  //     $("#comments").after(bigsquare2);
+  //     bigsquare2.wrap('<div style="float:right;"></div>');
+  //     (adsbygoogle = window.adsbygoogle || []).push({});
+  //   }
+  // }, 0);
 });
+if (location.protocol != 'https:' && location.host === 'robin-front.github.io') {
+ location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
